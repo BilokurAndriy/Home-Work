ef00f531045058ff063f92171e28a51973459b68
Andriy Bilokur <a.bilokur98@gmail.com>
2017-06-27 20:12:55 +0300

    домашня робота

---
+++ src/Home_Work.java
@@ -0,0 +1,10 @@
+/**
+ * Created by ANDREY on 27.06.2017.
+ */
+public class Home_Work {
+    public static void  main(String[] args)
+    {
+
+        System.out.println("Как я проведу лето 2017");
+    }
+}
